f78d0612e380177257a7561e527c0f6ba456a0a8
smk-141510181 <141510181@smkassalaambandung.sch.id>
2017-02-25 01:25:33 +0700

    Masih Gagal

---
+++ Android/app/src/main/java/assalaam/studio/ujikom/app/AppConfig.java
@@ -0,0 +1,9 @@
+package assalaam.studio.ujikom.app;
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "192.168.2.66/public/api/login";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://ujikom-2017.smkassalaambandung.sch.id/api/register";
+}
